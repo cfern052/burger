1d2458b507d12f0bd969c50e56f1f921c809447a
cfern052 <cfern052@gmail.com>
2019-03-18 14:26:02 -0400

    updated server info

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//static directory
+app.use(express.static("app/public"));
+
+//Route
+require("./controllers/burgers_controller.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
